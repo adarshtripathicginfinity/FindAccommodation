b9f0cdb15315cc9f29cc1f8a380727657a398636
Alok Srivastava <60250491+aloksrivastava7@users.noreply.github.com>
2023-05-29 19:06:34 +0530

    Page upload

--- accommodation/src/components/interestSent/interestSent.js
+++ accommodation/src/components/interestSent/interestSent.js
@@ -14,14 +14,6 @@ const InterestSent = (props) => {
         <Navbar />
         <Wrapper>
             <Container className="main-wrapper">
-            <div style={{margin:"3%"}}>    
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Library</li>
-                    </ol>
-                </nav>
-            </div>
             </Container>
         </Wrapper>
         </>
